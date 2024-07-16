327c7ba3b12318040be9540954cdaca6895f197e
noahk0 <noah.kim@mail.utoronto.ca>
2024-07-16 19:36:32 +0900

    Update #medium Valid Sudoku.py

--- #medium Valid Sudoku.py
+++ #medium Valid Sudoku.py
@@ -1,14 +1,12 @@
 def isValidSudoku(self, board: List[List[str]]) -> bool:
-    col = [set() for _ in range(9)]
-    box = [set() for _ in range(9)]
+    col, box = [set() for _ in range(9)], [set() for _ in range(9)]
         
     for i in range(9):
         row = set()
 
         for j in range(9):
             if board[i][j] != '.':
-                num = int(board[i][j])
-                idx = i // 3 * 3 + j // 3
+                num, idx = int(board[i][j]), i // 3 * 3 + j // 3
 
                 if num in row or num in col[j] or num in box[idx]:
                     return False
