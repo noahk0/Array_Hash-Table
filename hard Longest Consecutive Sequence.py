5422704ad62027ec17e6ee196152f21294b0b514
noahk0 <noah.kim@mail.utoronto.ca>
2024-07-16 19:37:17 +0900

    Update hard Longest Consecutive Sequence.py

--- hard Longest Consecutive Sequence.py
+++ hard Longest Consecutive Sequence.py
@@ -1,6 +1,5 @@
 def longestConsecutive(self, nums: List[int]) -> int:
-    longest = 0
-    nums = set(nums)
+    longest, nums = 0, set(nums)
 
     for num in nums:
         if (num - 1) not in nums:
