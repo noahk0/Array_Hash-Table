b56810dda8dcc85767a6e0032fb484fa01ba2e2a
noahk0 <noah.kim@mail.utoronto.ca>
2024-08-24 13:44:16 +0900

    Update hard Longest Consecutive Sequence O(n).py

--- hard Longest Consecutive Sequence O(n).py
+++ hard Longest Consecutive Sequence O(n).py
@@ -2,12 +2,14 @@ def longestConsecutive(self, nums: List[int]) -> int:
     longest, nums = 0, set(nums)
 
     for num in nums:
-        if (num - 1) not in nums:
-            consec = num + 1
+        if (num - 1) in nums:
+            continue
+            
+        consec = num + 1
 
-            while consec in nums:
-                consec += 1
+        while consec in nums:
+            consec += 1
 
-            longest = max(longest, consec - num)
+        longest = max(longest, consec - num)
 
     return longest
